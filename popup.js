6e1afa537d063a7450d0f852b6243d875a2b8a0e
DNAMcKnight <conner.cmk9040@gmail.com>
2023-02-15 23:19:23 +0600

    added events to update textarea

--- popup.js
+++ popup.js
@@ -1,4 +1,24 @@
-document.getElementById("text").oninput = function () {
-  document.getElementById("message").innerHTML =
-    document.getElementById("text").value.length;
+var textarea = document.getElementById('text');
+var message = document.getElementById('message');
+const submitButton = document.querySelector('input[type="submit"]');
+
+// when focused clear text
+textarea.onfocus = function () {
+  textarea.value = '';
+}
+
+//  when inputting expand window
+textarea.oninput = function () {
+  message.innerHTML = textarea.value.length;
+  textarea.style.height = "80";
+  textarea.style.height = Math.min(textarea.scrollHeight, 500) + "px";
 };
+
+
+submitButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  if (textarea.value === "What do you want me to read?" || "") {
+    textarea.value = "Text required!";
+  };
+  console.log('textValue');
+});
