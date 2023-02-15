6e8a8d55ba97ac49df724969776daac9e4ef3da8
DNAMcKnight <conner.cmk9040@gmail.com>
2023-02-16 00:25:25 +0600

    added audiocallback

--- popup.js
+++ popup.js
@@ -1,24 +1,42 @@
-var textarea = document.getElementById('text');
-var message = document.getElementById('message');
+var textarea = document.getElementById("text");
+var count = document.getElementById("count");
 const submitButton = document.querySelector('input[type="submit"]');
 
-// when focused clear text
-textarea.onfocus = function () {
-  textarea.value = '';
-}
-
-//  when inputting expand window
-textarea.oninput = function () {
-  message.innerHTML = textarea.value.length;
-  textarea.style.height = "80";
-  textarea.style.height = Math.min(textarea.scrollHeight, 500) + "px";
+// when focused clear text if the text is the default text
+textarea.onfocus = () => {
+  if (textarea.value.trim() === '') 
+    textarea.value = "";
 };
 
+//  when inputting expand window with a minimum size of 80
+textarea.oninput = () => {
+  count.innerHTML = textarea.value.length;
+  textarea.style.height = '80';
+  textarea.style.height = Math.min(textarea.scrollHeight, 450) + "px"
+};
 
-submitButton.addEventListener('click', function(event) {
+// when button is clicked the event triggers
+submitButton.addEventListener("click", (event) => {
   event.preventDefault();
-  if (textarea.value === "What do you want me to read?" || "") {
-    textarea.value = "Text required!";
-  };
-  console.log('textValue');
+  if (textarea.value.trim() === "") {
+    textarea.placeholder = "You must enter some text...";
+    textarea.value = "";
+    count.innerHTML = 0;
+  }
+  audioCallback();
 });
+
+async function audioCallback() {
+  let speak = await fetch('https://api.streamelements.com/kappa/v2/speech?voice=Brian&text=' + encodeURIComponent(textarea.value.trim()));
+  if (speak.status != 200) {
+    alert(await speak.text());
+    return;
+  }
+  let mp3 = await speak.blob();
+  let blobUrl = URL.createObjectURL(mp3)
+  document.getElementById('source').setAttribute('src', blobUrl);
+  let audio = document.getElementById('audio');
+  audio.pause();
+  audio.load();
+  audio.play();
+}
